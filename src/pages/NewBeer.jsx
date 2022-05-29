4bd257a48ec1e6b79463244f25473a42551b7976
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-30 01:51:22 +0200

    styling form

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -39,28 +39,36 @@ function NewBeer() {
         
         <Navbar/><br/>
         <h1>New Beer</h1>
-        <form>
-            <label htmlFor='name'>Name</label>
-            <input type='text' name='name' onChange={handleChange} value={state.name}/><br/>
-
-            <label htmlFor='tagline'>Tagline</label>
-            <input type='text' name='tagline' onChange={handleChange} value={state.tagline}/><br/>
-
-            <label htmlFor='description'>Description</label>
-            <input type='text' name='description' onChange={handleChange} value={state.description}/><br/>
-
-            <label htmlFor='first_brewed'>First brewed</label>
-            <input type='text' name='first_brewed' onChange={handleChange} value={state.first_brewed}/><br/>
-
-            <label htmlFor='brewers_tips'>Brewers tips</label>
-            <input type="text" name='brewers_tips' onChange={handleChange} value={state.brewers_tips}/><br/>
-
-            <label htmlFor='attenuation_level'>Attenuation level</label>
-            <input type='number' name='attenuation_level' onChange={handleChange} value={state.attenuation_level}/><br/>
-
-            <label htmlFor='contributed_by'>Contributed by</label>
-            <input type='text' name='contributed_by' onChange={handleChange} value={state.contributed_by}/><br/>
-            <button onClick={handleSubmit}>Add</button>
+        <form className='text-start'>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='name'>Name</label>
+                <input className="form-control" type='text' name='name' onChange={handleChange} value={state.name}/><br/>
+            </div>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='tagline'>Tagline</label>
+                <input className="form-control" type='text' name='tagline' onChange={handleChange} value={state.tagline}/><br/>
+            </div>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='description'>Description</label>
+                <input className="form-control" type='text' name='description' onChange={handleChange} value={state.description}/><br/>
+            </div>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='first_brewed'>First brewed</label>
+                <input className="form-control" type='text' name='first_brewed' onChange={handleChange} value={state.first_brewed}/><br/>
+            </div>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='brewers_tips'>Brewers tips</label>
+                <input className="form-control" type="text" name='brewers_tips' onChange={handleChange} value={state.brewers_tips}/><br/>
+            </div>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='attenuation_level'>Attenuation level</label>
+                <input className="form-control" type='number' name='attenuation_level' onChange={handleChange} value={state.attenuation_level}/><br/>
+            </div>
+            <div className='mb-3'>
+                <label className="form-label" htmlFor='contributed_by'>Contributed by</label>
+                <input className="form-control" type='text' name='contributed_by' onChange={handleChange} value={state.contributed_by}/><br/>
+            </div>
+                <button onClick={handleSubmit}>Add</button>
         </form>
     </div>
   )
